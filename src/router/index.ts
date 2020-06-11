dff6c14b12a9d648761533feb6a742925e367575
Jakub Jan Strebeyko <strebeyko@gmx.com>
2020-06-11 23:25:17 +0200

    de-childrenize routes, delete Default

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,6 @@
 import Vue from 'vue';
 import VueRouter, { RouteConfig } from 'vue-router';
 import HomePage from '@/views/Home/index.vue';
-import Default from '@/views/Home/Default.vue';
-import SearchResults from '@/views/SearchResults/index.vue';
 
 Vue.use(VueRouter);
 
@@ -11,29 +9,18 @@ const routes: Array<RouteConfig> = [
     path: '/',
     name: 'Home',
     component: HomePage,
-    children: [
-      {
-        path: '/search/:phrase',
-        name: 'Search',
-        component: SearchResults,
-        props: true,
-      },
-      {
-        path: '/details/:id',
-        name: 'Details',
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: () => import(/* webpackChunkName: "about" */ '../views/Details/index.vue'),
-        props: true,
-      },
-      {
-        path: '*',
-        name: 'All',
-        component: Default,
-        props: true,
-      },
-    ],
+  },
+  {
+    path: '/search/:phrase',
+    name: 'Search',
+    component: () => import(/* webpackChunkName: "about" */ '../views/SearchResults/index.vue'),
+    props: true,
+  },
+  {
+    path: '/details/:id',
+    name: 'Details',
+    component: () => import(/* webpackChunkName: "about" */ '../views/Details/index.vue'),
+    props: true,
   },
 ];
 
