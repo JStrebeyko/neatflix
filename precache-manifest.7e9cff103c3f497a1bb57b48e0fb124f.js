190944551dadfe0af92dbf9b9002acd2d153959e
Jakub Jan Strebeyko <strebeyko@gmx.com>
2020-06-28 19:29:02 +0200

    deploy

---
+++ precache-manifest.7e9cff103c3f497a1bb57b48e0fb124f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e501dbe1eef4c0445939",
+    "url": "/neatflix/css/about.132d2d9f.css"
+  },
+  {
+    "revision": "6d899b55250a3b270973",
+    "url": "/neatflix/css/app.0f216fa4.css"
+  },
+  {
+    "revision": "ba31eb142f63306babeb6272d4943a9b",
+    "url": "/neatflix/index.html"
+  },
+  {
+    "revision": "e501dbe1eef4c0445939",
+    "url": "/neatflix/js/about.b759db9b.js"
+  },
+  {
+    "revision": "6d899b55250a3b270973",
+    "url": "/neatflix/js/app.59646ceb.js"
+  },
+  {
+    "revision": "47022c6e56fd4f47cd19",
+    "url": "/neatflix/js/chunk-vendors.0229ab61.js"
+  },
+  {
+    "revision": "9d46a4d55ef5e4ff7690d8d783624e89",
+    "url": "/neatflix/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/neatflix/robots.txt"
+  }
+]);
